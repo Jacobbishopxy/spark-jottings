5fa24a35f5c839e21dbf9ff9ebd78060318a7180
jacob <jacobbishopxy@gmail.com>
2022-09-04 01:39:17 +0800

    chore

--- src/main/scala/jotting/MyJdbcUtils.scala
+++ src/main/scala/jotting/MyJdbcUtils.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.execution.datasources.jdbc.JdbcOptionsInWrite
 import org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils
 import org.apache.spark.sql.jdbc.JdbcDialects
 import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.connector.catalog.TableChange
 
 class MyJdbcUtils(conn: Jotting.Conn, connectionOpt: Option[Connection] = None) {
 
@@ -27,15 +28,43 @@ class MyJdbcUtils(conn: Jotting.Conn, connectionOpt: Option[Connection] = None) 
     new JdbcOptionsInWrite(op)
   }
 
-  private def jdbcOptionsAddTable(table: String): JdbcOptionsInWrite = {
+  private def jdbcOptionsAddTable(table: String): JdbcOptionsInWrite =
     jdbcOptionsAddOptions(("dbtable", table))
-  }
 
   private def jdbcOptionsAddTruncateTable(
       table: String,
       cascade: Boolean = false
+  ): JdbcOptionsInWrite =
+    jdbcOptionsAddOptions(
+      ("dbtable", table),
+      ("cascadeTruncate", cascade.toString())
+    )
+
+  private def jdbcOptionsAddSaveTable(
+      table: String,
+      batchSize: Option[Integer] = None,
+      isolationLevel: Option[String] = None,
+      numPartition: Option[Integer] = None
   ): JdbcOptionsInWrite = {
-    jdbcOptionsAddOptions(("dbtable", table), ("cascadeTruncate", cascade.toString()))
+    val op = conn.options ++
+      batchSize.map(v => ("batchsize" -> v.toString())) ++
+      isolationLevel.map(v => ("isolationLevel" -> v)) ++
+      numPartition.map(v => ("numPartitions" -> v.toString()))
+
+    new JdbcOptionsInWrite(op)
+  }
+
+  private def jdbcOptionsAddCreateTable(
+      createTableColumnTypes: Option[String],
+      createTableOptions: Option[String],
+      tableComment: Option[String]
+  ): JdbcOptionsInWrite = {
+    val op = conn.options ++
+      createTableColumnTypes.map(("createTableColumnTypes" -> _)) ++
+      createTableOptions.map(("createTableOptions" -> _)) ++
+      tableComment.map(("tableComment" -> _))
+
+    new JdbcOptionsInWrite(op)
   }
 
   /** Raw statement for saving a DataFrame
@@ -229,7 +258,6 @@ class MyJdbcUtils(conn: Jotting.Conn, connectionOpt: Option[Connection] = None) 
       conflictColumns: Seq[String],
       conflictAction: String
   ): Unit = {
-
     val ca = conflictAction match {
       case "doNothing" => UpsertAction.DoNothing
       case "doUpdate"  => UpsertAction.DoUpdate
@@ -284,20 +312,126 @@ class MyJdbcUtils(conn: Jotting.Conn, connectionOpt: Option[Connection] = None) 
     JdbcUtils.executeQuery(connection, jdbcOptions, query)(_ => {})
   }
 
+  /** Check if a table exists
+    *
+    * @param table
+    * @return
+    */
   def tableExists(table: String): Boolean =
     JdbcUtils.tableExists(connection, jdbcOptionsAddTable(table))
 
+  /** Drop a table
+    *
+    * @param table
+    */
   def dropTable(table: String): Unit =
     JdbcUtils.dropTable(connection, table, jdbcOptions)
 
+  /** Truncate a table
+    *
+    * @param table
+    * @param cascadeTruncate
+    */
   def truncateTable(table: String, cascadeTruncate: Boolean = false): Unit =
     JdbcUtils.truncateTable(connection, jdbcOptionsAddTruncateTable(table, cascadeTruncate))
 
+  /** Get a table's schema if it exists
+    *
+    * @param table
+    * @return
+    */
   def getSchemaOption(table: String): Option[StructType] = {
     JdbcUtils.getSchemaOption(connection, jdbcOptionsAddTable(table))
   }
 
+  /** Save a DataFrame to the database in a single transaction
+    *
+    * @param df
+    * @param table
+    * @param isCaseSensitive
+    * @param batchSize
+    * @param isolationLevel
+    * @param numPartition
+    */
+  def saveTable(
+      df: DataFrame,
+      table: String,
+      isCaseSensitive: Boolean,
+      batchSize: Option[Integer] = None,
+      isolationLevel: Option[String] = None,
+      numPartition: Option[Integer] = None
+  ): Unit =
+    JdbcUtils.saveTable(
+      df,
+      None,
+      isCaseSensitive,
+      jdbcOptionsAddSaveTable(
+        table,
+        batchSize,
+        isolationLevel,
+        numPartition
+      )
+    )
+
+  /** Create a table with a given schema
+    *
+    * @param table
+    * @param schema
+    * @param isCaseSensitive
+    * @param createTableColumnTypes
+    * @param createTableOptions
+    * @param tableComment
+    */
+  def createTable(
+      table: String,
+      schema: StructType,
+      isCaseSensitive: Boolean,
+      createTableColumnTypes: Option[String],
+      createTableOptions: Option[String],
+      tableComment: Option[String]
+  ): Unit =
+    JdbcUtils.createTable(
+      connection,
+      table,
+      schema,
+      isCaseSensitive,
+      jdbcOptionsAddCreateTable(
+        createTableColumnTypes,
+        createTableOptions,
+        tableComment
+      )
+    )
+
+  /** Rename a table from the JDBC database
+    *
+    * @param oldTable
+    * @param newTable
+    */
+  def renameTable(oldTable: String, newTable: String): Unit =
+    JdbcUtils.renameTable(connection, oldTable, newTable, jdbcOptions)
+
+  /** Update a table from the JDBC database
+    *
+    * @param table
+    * @param changes
+    */
+  def alterTable(table: String, changes: Seq[TableChange]): Unit =
+    JdbcUtils.alterTable(connection, table, changes, jdbcOptions)
+
   // TODO:
+  // createSchema
+  // schemaExists
+  // listSchemas
+  // alterSchemaComment
+  // removeSchemaComment
+  // dropSchema
+
+  // TODO:
+  // createIndex
+  // indexExists
+  // dropIndex
+  // listIndexes
+  // checkIfIndexExists
 }
 
 object MyJdbcUtils {
