d3ee809c60007501dadee806fa81ba257589fb29
jacob <jacobbishopxy@gmail.com>
2022-09-11 21:07:13 +0800

    chore

--- src/main/scala/jotting/simple/Job1.scala
+++ src/main/scala/jotting/simple/Job1.scala
@@ -3,18 +3,31 @@ package jotting.simple
 import java.util.Random
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
 
 object Job1 {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
+    implicit val spark = SparkSession.builder
       .appName("GroupBy Test")
       .getOrCreate()
 
-    val log = LogManager.getRootLogger
-    log.setLevel(Level.INFO)
+    args.toList match {
+      case "countingNumbers" :: tail =>
+        countingNumbers(tail: _*)
+      case "datetimeFormatConversion" :: _ =>
+        datetimeFormatConversion()
+      case a @ _ => throw new Exception(s"unsupported args: $a")
+    }
 
+    spark.stop()
+  }
+
+  val log = LogManager.getRootLogger
+  log.setLevel(Level.INFO)
+
+  private def countingNumbers(args: String*)(implicit spark: SparkSession): Unit = {
     log.info("Setting variables...")
 
     val numMappers  = if (args.length > 0) args(0).toInt else 2
@@ -42,7 +55,30 @@ object Job1 {
     val res = pairs1.groupByKey(numReducers).count()
     println(s"res: $res")
     log.info(s"result: $res")
+  }
 
-    spark.stop()
+  private def datetimeFormatConversion()(implicit spark: SparkSession): Unit = {
+    import spark.implicits._
+
+    val format = "yyyyMMddHHmmss"
+
+    val data = Seq(
+      (20171230000010L, "ax"),
+      (20171230000010L, "aj"),
+      (20180101000100L, "ax"),
+      (20180101000100L, "aj"),
+      (20180203001000L, "ax"),
+      (20180203001000L, "aj"),
+      (20180321000100L, "ax"),
+      (20180321000100L, "aj"),
+      (20190510000010L, "ax"),
+      (20190510000010L, "aj"),
+      (20191002000001L, "ax"),
+      (20191002000001L, "aj")
+    )
+    val df = spark.createDataFrame(data).toDF("date", "symbol")
+
+    val res = df.withColumn("datetype_timestamp", to_timestamp($"date".cast("String"), format))
+    res.show()
   }
 }
